f2c46e6553f9d85b002f5be4fc720e615762668e
Tomilola-ng <tee.o2809@gmail.com>
2024-02-17 13:14:45 +0100

    ENABLED_COURSE_AND_ADDED_ROUTE_API_PREFIX

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from "./app.module";
+import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: "http://localhost:3000/" });
+  app.setGlobalPrefix("api");
   await app.listen(3000);
 }
 bootstrap();
