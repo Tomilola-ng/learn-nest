bb68623e3ae389ac8d3cfbad9dd49712a2ea9511
Tomilola-ng <tee.o2809@gmail.com>
2024-02-17 13:15:19 +0100

    ADDED_CORS_WILD_CARD

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from "@nestjs/core";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: "http://localhost:3000/" });
+  app.enableCors({ origin: "*" });
   app.setGlobalPrefix("api");
   await app.listen(3000);
 }
