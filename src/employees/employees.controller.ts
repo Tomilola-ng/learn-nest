96da343a84a3aaa4199795acdba38d276c93c36f
Tomilola-ng <tee.o2809@gmail.com>
2024-02-17 14:46:12 +0100

    LOGGER_LOGGING_CUTOM

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -7,14 +7,17 @@ import {
   Param,
   Delete,
   Query,
+  Ip,
 } from "@nestjs/common";
 import { EmployeesService } from "./employees.service";
-import { Prisma } from "@prisma/client";
+import { Prisma, Role } from "@prisma/client";
 import { SkipThrottle } from "@nestjs/throttler";
+import { MyLoggerService } from "src/my-logger/my-logger.service";
 
 @Controller("employees")
 export class EmployeesController {
   constructor(private readonly employeesService: EmployeesService) {}
+  private readonly logger = new MyLoggerService(EmployeesController.name);
 
   @Post()
   create(@Body() createEmployeeDto: Prisma.EmployeeCreateInput) {
@@ -23,7 +26,12 @@ export class EmployeesController {
 
   @SkipThrottle()
   @Get()
-  findAll(@Query("role") role?: "INTERN" | "ADMIN" | "ENGINEER") {
+  findAll(@Ip() ip: string, @Query("role") role?: Role) {
+    this.logger.log(
+      `Request for ALL Employees\t${ip}`,
+      EmployeesController.name
+    );
+
     return this.employeesService.findAll(role);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { AppModule } from "./app.module";
 import { NestFactory } from "@nestjs/core";
+import { MyLoggerService } from "./my-logger/my-logger.service";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(app.get(MyLoggerService));
   app.enableCors({ origin: "*" });
   app.setGlobalPrefix("api");
   await app.listen(3000);
